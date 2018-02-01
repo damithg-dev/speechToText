d2b400cf17038f7bb9b0a50fda926fc98f3b260b
Gayanath Damith Amarasinghe <d.amarasinghe@hotmail.com>
2018-02-01 11:57:52 +0530

    speech to text

---
+++ SpechToText/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  SpechToText
+//
+//  Created by Gayanath Damith Amarasinghe on 2/1/18.
+//  Copyright © 2018 Gayanath Damith Amarasinghe. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
